305bbe9ec6a63f5a52cd18f8e22eedfed93185cd
BLink <60574144+BPisTheDestiny@users.noreply.github.com>
2021-04-08 00:03:47 +0800

    Rename zidongshengchengshudu.cpp to Auto-generation.cpp

--- Lab1/Auto-generation.cpp
+++ Lab1/Auto-generation.cpp
@@ -18,24 +18,24 @@ void clear()
  } 
 bool set(int x, int y, int val)
 {
-    if (sudo[y][x] != 0)        //·Ç¿Õ
+    if (sudo[y][x] != 0)        //éç©º
         return false;
     int x0, y0;
     for (x0=0; x0<9; x0++)
     {
-        if (sudo[y][x0] == val) //ĞĞ³åÍ»
+        if (sudo[y][x0] == val) //è¡Œå†²çª
             return false;
     }
     for (y0=0; y0<9; y0++)
     {
-        if (sudo[y0][x] == val) //ÁĞ³åÍ»
+        if (sudo[y0][x] == val) //åˆ—å†²çª
             return false;
     }
     for (y0=y/3*3; y0<y/3*3+3; y0++)
     {
         for (x0=x/3*3; x0<x/3*3+3; x0++)
         {
-            if (sudo[y0][x0] == val) //¸ñ³åÍ»
+            if (sudo[y0][x0] == val) //æ ¼å†²çª
                 return false;
         }
     }
@@ -48,7 +48,7 @@ void reset(int x, int y)
     sudo[y][x] = 0;
 }
  
-void initXOrd(int* xOrd)    //0~9Ëæ»úĞòÁĞ
+void initXOrd(int* xOrd)    //0~9éšæœºåºåˆ—
 {
     int i, k, tmp;
     for (i=0; i<9; i++)
@@ -67,24 +67,24 @@ void initXOrd(int* xOrd)    //0~9Ëæ»úĞòÁĞ
 bool fillFrom(int y, int val)
 {
     int xOrd[9];
-    initXOrd(xOrd);     //Éú³Éµ±Ç°ĞĞµÄÉ¨ÃèĞòÁĞ
+    initXOrd(xOrd);     //ç”Ÿæˆå½“å‰è¡Œçš„æ‰«æåºåˆ—
     for (int i=0; i<9; i++)
     {
         int x = xOrd[i];
         if (set(x, y, val))
         {
-            if (y == 8)                 //µ½ÁË×îºóÒ»ĞĞ
+            if (y == 8)                 //åˆ°äº†æœ€åä¸€è¡Œ
             {
-                //µ±Ç°Ìî9Ôò½áÊø, ·ñÔò´ÓµÚÒ»ĞĞÌîÏÂÒ»¸öÊı
+                //å½“å‰å¡«9åˆ™ç»“æŸ, å¦åˆ™ä»ç¬¬ä¸€è¡Œå¡«ä¸‹ä¸€ä¸ªæ•°
                 if (val == 9 || fillFrom(0, val+1)) 
                     return true;
             } 
             else
             {
-                if (fillFrom(y+1, val)) //ÏÂÒ»ĞĞ¼ÌĞøÌîµ±Ç°Êı
+                if (fillFrom(y+1, val)) //ä¸‹ä¸€è¡Œç»§ç»­å¡«å½“å‰æ•°
                     return true;
             }
-            reset(x, y);    //»ØËİ
+            reset(x, y);    //å›æº¯
         }
     }
     return false;
@@ -99,7 +99,7 @@ void digHole(int holeCnt)
         hole[i / 9][i % 9] = 0;
         idx[i] = i;
     }
-    for (i=0; i<holeCnt; i++)    //Ëæ»úÍÚ¶´Î»ÖÃ
+    for (i=0; i<holeCnt; i++)    //éšæœºæŒ–æ´ä½ç½®
     {
         k = rand() % 81;
         int tmp = idx[k];
