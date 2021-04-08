16816ab244609262d1f2b91699d0775f2eb8b2d7
Zyn0714 <1592724941@qq.com>
2021-04-09 00:09:59 +0800

    重新上传，修改说明文件以及部分细节

---
+++ lab1/basic_dyn/main.cc
@@ -0,0 +1,141 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "sudoku.h"
+
+char puzzle[num_of_puzzle][128];
+char solution[num_of_puzzle][N+1];
+int board[num_of_puzzle][N];
+int spaces[num_of_puzzle][N];
+
+int total=0;//数独总数量
+int total_solved = 0;//解决数独的数目
+bool (*solve)(int, int, int*, int*) = solve_sudoku_basic;//求解数独的方式
+int thread_count=2;//，线程的数目，默认双线程
+pthread_t* thread_handles;
+long int thread;
+
+int nextPuzzleToBeDone=0;
+pthread_mutex_t puzzleQueueMutex=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+int64_t now()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+void init_data()
+{ //离线
+	char *file_name=(char*)malloc(256*sizeof(char));
+	FILE *fp;
+	while(fgets(file_name, 256, stdin))
+	{
+		if(file_name[0]=='\n')
+		{
+			printf("input end.start solve.please wait\n");
+			break;
+		}
+		if(file_name[strlen(file_name)-1]=='\n')
+        {
+		    file_name[strlen(file_name)-1]='\0';
+        }
+		fp = fopen(file_name, "r");
+		if(fp==NULL)
+		{
+			printf("%s does not exist.please try again\n",file_name);
+			continue;
+		}
+		while(fgets(puzzle[total],1024,fp))
+		{
+			if(strlen(puzzle[total])>=N)
+			{
+				++total;
+			}
+		}
+	}
+}
+int recvAPuzzle()
+{
+	int currentPuzzleID=0;
+	pthread_mutex_lock(&puzzleQueueMutex);
+	if(nextPuzzleToBeDone>=total) 
+	{
+		pthread_mutex_unlock(&puzzleQueueMutex);
+		return -1;
+	}
+	currentPuzzleID=nextPuzzleToBeDone;
+	nextPuzzleToBeDone++;
+	pthread_mutex_unlock(&puzzleQueueMutex);
+	return currentPuzzleID;
+}
+void* Pth_solve(void* rank)
+{
+	long int my_rank = (long int) rank;
+	int currentPuzzleID=0;
+	int *whichPuzzleIHaveDone=(int*)malloc(total*sizeof(int));//Remember which job I have done
+	long int numOfPuzzleIHaveDone=0;//Remember how many jobs I have done
+	while(true)
+	{
+		currentPuzzleID = recvAPuzzle();
+		if(currentPuzzleID==-1)//All puzzle done!
+			break;
+		whichPuzzleIHaveDone[numOfPuzzleIHaveDone]=currentPuzzleID;
+		numOfPuzzleIHaveDone++;
+		int nspaces = input(puzzle[currentPuzzleID],board[currentPuzzleID],spaces[currentPuzzleID]);
+		if(solve(0,nspaces,board[currentPuzzleID],spaces[currentPuzzleID]))
+		{
+			++total_solved;
+			if (!solved(board[currentPuzzleID]));
+		} else{
+			printf("currentPuzzleID:%d;No: %s", currentPuzzleID,puzzle[currentPuzzleID]);
+		}
+		for(int j=0; j<N; ++j)
+		{
+			solution[currentPuzzleID][j]=board[currentPuzzleID][j]+'0';
+		}
+		solution[currentPuzzleID][N]='\0';
+	}
+	return NULL;
+}
+void cout_solution()
+{
+	FILE *fp = fopen("outfile","w");
+	for(int i=0; i<total; ++i) {
+		fprintf(fp,"%s\n",solution[i]);
+	}
+	printf("all the solution has been output to 'outfile'\n");
+}
+int main(int argc, char* argv[]) {
+
+	if(argv[1]!=NULL)
+    {
+	    thread_count = strtol(argv[1],NULL,10);//可将线程数作为参数输入
+    }
+
+	init_neighbors();
+	init_data();
+
+	int64_t start = now();//开始计时
+	thread_handles = (pthread_t *)malloc(thread_count*sizeof(pthread_t));
+
+	for(thread=0; thread<thread_count; ++thread)
+	{
+		pthread_create(&thread_handles[thread],NULL,Pth_solve,(void*)thread);
+	}
+	for(thread=0; thread<thread_count; ++thread)
+	{
+		pthread_join(thread_handles[thread],NULL);
+	}
+	
+	int64_t end = now();
+	double sec = (end-start)/1000000.0;
+	printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
+	free(thread_handles);
+	cout_solution();
+}
+
